1c285fc65159bdd47accf3e161bc2c3b24ab20ad
Asirio <75069152+Ryirio@users.noreply.github.com>
2022-11-10 22:38:38 +0700

    /gospel

---
+++ src/commands/verse.js
@@ -0,0 +1,20 @@
+/*
+ * This is the /purge command. It deletes messages
+ *
+ * Made with <3 by Jason and Jovan
+ *
+ * Copyright (c) Pix3l_ 2022
+ * Code is licensed under MIT
+ */
+
+// Import the required modules
+const { SlashCommandBuilder } = require("discord.js");
+
+// Export the command data for loader
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("wisewords")
+        .setDescription("Wise words"),
+
+    async execute(interaction) {}
+}
