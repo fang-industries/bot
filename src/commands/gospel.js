5f607c836cf8083ac0c025b40e21f441fa9cc18a
jason <pnxl@pm.me>
2022-11-10 23:46:16 +0700

    comment gospel

--- src/commands/gospel.js
+++ src/commands/gospel.js
@@ -1,5 +1,6 @@
 /*
- * This is the /purge command. It deletes messages
+ * This is the /gospel command. It spits out random
+ * parodies of bible verses. We stayed up for this.
  *
  * Made with <3 by Jason and Jovan
  *
@@ -13,16 +14,30 @@ const verses = require("../miscellaneous/assets/verses.json");
 
 // Export the command data for loader
 module.exports = {
+  /*
+   * Command      : /gospel
+   * Description  : Some words of wisdom from the almighty chicken God.
+   *
+   * What it does : i honestly don't know anymore man
+   */
+
+  // Define data for loader
   data: new SlashCommandBuilder()
     .setName("gospel")
     .setDescription("Some words of wisdom from the almighty chicken God."),
 
+  // Execute the command asynchronously
   async execute(interaction) {
+    // Select a random verse
     const randomVerse = verses[Math.floor(Math.random() * verses.length)];
+
+    // Create embed
     const embed = EmbedBuilder()
-        .setColor(0xF1B581)
-        .setTitle(`🐔 ${randomVerse.title} 🐔`)
-        .setDescription(randomVerse.content)
-    await interaction.reply({ embeds: [embed] })
+      .setColor(0xf1b581)
+      .setTitle(`🐔 ${randomVerse.title} 🐔`)
+      .setDescription(randomVerse.content);
+
+    // Respond to the user with the embed
+    await interaction.reply({ embeds: [embed] });
   },
 };
