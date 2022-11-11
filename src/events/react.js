fd639a73b7509c5c454aceb35c52e5936d81c640
Asirio <75069152+Ryirio@users.noreply.github.com>
2022-11-11 21:20:18 +0700

    react.js

---
+++ src/events/react.js
@@ -0,0 +1,12 @@
+const { Events, MessageReaction } = require("discord.js")
+module.exports = {
+    name: Event.message,
+    async execute(message) {
+        const guildMember = interaction.guild.memberCount
+        const foundingFather = "1040631470852677744"
+        if (MessageReaction.count > guildMember / 2){
+        }
+    }
+
+
+}
