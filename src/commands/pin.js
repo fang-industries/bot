c862be4266a6fdc7cd2b00ae1f8a3bd2d99d02bd
Asirio <75069152+Ryirio@users.noreply.github.com>
2022-11-14 09:07:27 +0700

    n

--- src/commands/pin.js
+++ src/commands/pin.js
@@ -8,12 +8,12 @@ module.exports = {
         .setDescription("Pins the message to the #hall-of-fame channel")
         .addStringOption((opt) =>
             opt
-                .setName("MsgID")
+                .setName("ID")
                 .setDescription("Set the message you want to forward.")
         ),
 
     async execute(interaction) {
-        const msgID = interaction.options.getString('MsgID')
+        const msgID = interaction.options.getString("ID")
         const channelID = interaction.guild.channels.find(ch => ch.name === 'hall-of-fame')
         const fetchmsg = interaction.channel.fetchMessage(msgID)
         await console.log(fetchmsg)
